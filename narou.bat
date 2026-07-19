954b5307307eb0fa7e99d0342576c9de7cd508d2
lpubsppop01 <lpubsppop01@gmail.com>
2026-07-19 22:07:35 +0900

    narou.batでbundle execを使用するように

--- narou.bat
+++ narou.bat
@@ -1,10 +1,12 @@
 @echo off
 REM -*- mode: ruby -*-
+setlocal
+set BUNDLE_GEMFILE=%~dp0\Gemfile
 @if not "%~d0" == "~d0" goto WinNT
-ruby -x narou.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
+bundle exec ruby -x narou.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
 @goto endofruby
 :WinNT
-ruby -x "%~f0" %*
+bundle exec ruby -x "%~f0" %*
 @goto endofruby
 #! ruby
 
@@ -12,3 +14,4 @@ require_relative "narou.rb"
 
 __END__
 :endofruby
+endlocal
